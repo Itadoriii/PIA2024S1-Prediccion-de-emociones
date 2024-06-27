7c80fcb9a6906849582a00131c991af9cfe16a8f
Itadoriii <86499855+Itadoriii@users.noreply.github.com>
2024-06-27 00:09:05 -0400

    magna core

--- app.py
+++ app.py
@@ -3,7 +3,195 @@ from flask import Flask, request, render_template
 from transformers import RobertaForSequenceClassification, RobertaTokenizer
 import torch
 import pandas as pd
-
+emotions = [
+    {
+      "nombre": "admiración",
+      "50": "Influencia",
+      "30": "Percepción y Comprensión Emocional",
+      "20": "Desarrollar y Estimular a los demás"
+    },
+    {
+      "nombre": "alegría",
+      "50": "Autoconciencia Emocional",
+      "30": "Autocontrol Emocional",
+      "20": "Motivación al Logro"
+    },
+    {
+      "nombre": "amor",
+      "50": "Desarrollo de la Relaciones",
+      "30": "Desarrollar y Estimular a los demás",
+      "20": "Autoconciencia Emocional"
+    },
+    {
+      "nombre": "ansiedad",
+      "50": "Adaptabilidad",
+      "30": "Autocontrol Emocional",
+      "20": "Tolerancia a la Frustración"
+    },
+    {
+      "nombre": "apoyo",
+      "50": "Colaboración y Cooperación",
+      "30": "Influencia",
+      "20": "Liderazgo"
+    },
+    {
+      "nombre": "arrepentimiento",
+      "50": "Tolerancia a la Frustración",
+      "30": "Autoconciencia Emocional",
+      "20": "Adaptabilidad"
+    },
+    {
+      "nombre": "asombro",
+      "50": "Adaptabilidad",
+      "30": "Autocontrol Emocional",
+      "20": "Autoconciencia Emocional"
+    },
+    {
+      "nombre": "cansancio",
+      "50": "Autoconciencia Emocional",
+      "30": "Adaptabilidad",
+      "20": "Motivación al Logro"
+    },
+    {
+      "nombre": "confusion",
+      "50": "Autoconciencia Emocional",
+      "30": "Asertividad",
+      "20": "Adaptabilidad"
+    },
+    {
+      "nombre": "culpa",
+      "50": "Influencia",
+      "30": "Empatía",
+      "20": "Comprensión Organizativa"
+    },
+    {
+      "nombre": "curiosidad",
+      "50": "Desarrollar y Estimular a los demás",
+      "30": "Conciencia Crítica",
+      "20": "Comprensión Organizativa"
+    },
+    {
+      "nombre": "decepcion",
+      "50": "Desarrollo de la Relaciones",
+      "30": "Desarrollar y Estimular a los demás",
+      "20": "Percepción y Comprensión Emocional"
+    },
+    {
+      "nombre": "desden",
+      "50": "Desarrollo de la Relaciones",
+      "30": "Colaboración y Cooperación",
+      "20": "Manejo de Conflictos"
+    },
+    {
+      "nombre": "deseo",
+      "50": "Motivación al Logro",
+      "30": "Autocontrol Emocional",
+      "20": "Autoconciencia Emocional"
+    },
+    {
+      "nombre": "desesperacion",
+      "50": "Autocontrol Emocional",
+      "30": "Tolerancia a la Frustración",
+      "20": "Asertividad"
+    },
+    {
+      "nombre": "dolor",
+      "50": "Autocontrol Emocional",
+      "30": "Autoestima",
+      "20": "Tolerancia a la Frustración"
+    },
+    {
+      "nombre": "duda",
+      "50": "Motivación al Logro",
+      "30": "Asertividad",
+      "20": "Adaptabilidad"
+    },
+    {
+      "nombre": "frustacion",
+      "50": "Tolerancia a la Frustración",
+      "30": "Adaptabilidad",
+      "20": "Autoestima"
+    },
+    {
+      "nombre": "honestidad",
+      "50": "Comunicación Asertiva",
+      "30": "Colaboración y Cooperación",
+      "20": "Liderazgo"
+    },
+    {
+      "nombre": "impresion",
+      "50": "Autoconciencia Emocional",
+      "30": "Comprensión Organizativa",
+      "20": "Percepción y Comprensión Emocional"
+    },
+    {
+      "nombre": "indignacion",
+      "50": "Manejo de Conflictos",
+      "30": "Liderazgo",
+      "20": "Percepción y Comprensión Emocional"
+    },
+    {
+      "nombre": "interes",
+      "50": "Percepción y Comprensión Emocional",
+      "30": "Influencia",
+      "20": "Liderazgo"
+    },
+    {
+      "nombre": "molestia",
+      "50": "Desarrollar y Estimular a los demás",
+      "30": "Autoconciencia Emocional",
+      "20": "Desarrollo de la Relaciones"
+    },
+    {
+      "nombre": "odio",
+      "50": "Desarrollar y Estimular a los demás",
+      "30": "Empatía",
+      "20": "Autocontrol Emocional"
+    },
+    {
+      "nombre": "orgullo",
+      "50": "Autoestima",
+      "30": "Liderazgo",
+      "20": "Influencia"
+    },
+    {
+      "nombre": "preocupacion",
+      "50": "Empatía",
+      "30": "Percepción y Comprensión Emocional",
+      "20": "Desarrollar y Estimular a los demás"
+    },
+    {
+      "nombre": "remordimiento",
+      "50": "Tolerancia a la Frustración",
+      "30": "Manejo de Conflictos",
+      "20": "Adaptabilidad"
+    },
+    {
+      "nombre": "sorpresa",
+      "50": "Desarrollar y Estimular a los demás",
+      "30": "Adaptabilidad",
+      "20": "Percepción y Comprensión Emocional"
+    },
+    {
+      "nombre": "tranquilidad",
+      "50": "Autocontrol Emocional",
+      "30": "Tolerancia a la Frustración",
+      "20": "Adaptabilidad"
+    },
+    {
+      "nombre": "tristeza",
+      "50": "Autoconciencia Emocional",
+      "30": "Autocontrol Emocional",
+      "20": "Tolerancia a la Frustración"
+    },
+    {
+      "nombre": "verguenza",
+      "50": "Autoconciencia Emocional",
+      "30": "Autoestima",
+      "20": "Autocontrol Emocional"
+    }
+    
+]
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
@@ -41,8 +229,8 @@ def create_app(test_config=None):
         if request.method == 'POST':
             user_input = request.form['user_input']
             # Aquí llamamos a la función que procesa el input
-            prediction, user_text = run_inference2(user_input)
-            return render_template('/predict2.html', prediction=prediction, user_text=user_text)
+            prediction, user_text, factor_50, factor_30, factor_20 = run_inference2(user_input)
+            return render_template('/predict2.html', prediction=prediction, user_text=user_text, factor_50=factor_50, factor_30=factor_30, factor_20=factor_20)
 
         return render_template('/predict2.html')
 
@@ -110,7 +298,19 @@ def run_inference2(text):
 
     # Decodificar la predicción
     emotion = emotion_categories[predictions]
-    return emotion, text
+
+    # Buscar la emoción en el JSON
+    emotion_data = next((item for item in emotions if item["nombre"] == emotion), None)
+    
+    if emotion_data:
+        factor_50 = emotion_data["50"]
+        factor_30 = emotion_data["30"]
+        factor_20 = emotion_data["20"]
+    else:
+        factor_50 = factor_30 = factor_20 = "No disponible"
+
+    return emotion, text, factor_50, factor_30, factor_20
+
 
 if __name__ == '__main__':
     app = create_app()
